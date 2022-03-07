3a2550d7593a73d56461303afcb9558e318c92bc
jjyem <jjye_moon@megatus.com>
2022-03-07 16:17:27 +0900

    server 구성

--- sp01/src/main/java/controller/MainController.java
+++ sp01/src/main/java/controller/MainController.java
@@ -1,10 +1,32 @@
 package controller;
 
+import java.util.Locale;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
 public class MainController {
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+	
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public MainController() {
+		logger.info("MainController");
+		logger.info("start test Server...");
 	}
-
+	
+	@RequestMapping(value = {"", "/", "index"}, method = RequestMethod.GET)
+    public String index(HttpServletRequest HttpRequest, HttpSession session, Locale locale, Model model) {
+		logger.info("index page...");
+		
+		return "index";
+    }
+	
 }
